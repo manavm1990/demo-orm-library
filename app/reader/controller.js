08b1e10f569a7bc410db5ceb6c4cb0183a361216
Manav Misra <manavm@visionwebsoft.com>
2023-01-12 21:57:11 -0600

    Add reader controller

---
+++ app/reader/controller.js
@@ -0,0 +1,19 @@
+import Book from "../book/model.js";
+import Card from "../card/model.js";
+import Reader from "./model.js";
+
+export default {
+  create(payload) {
+    return Reader.create(payload);
+  },
+
+  show(id) {
+    return Reader.findByPk(id, {
+      include: [Book, Card],
+    });
+  },
+
+  delete(id) {
+    return Reader.destroy({ where: { id } });
+  },
+};

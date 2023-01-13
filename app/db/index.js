eba446badde2616165acd6dcbceb046a2d75c71d
Manav Misra <manavm@visionwebsoft.com>
2023-01-12 21:31:02 -0600

    docs: :bulb:

--- app/db/index.js
+++ app/db/index.js
@@ -6,13 +6,18 @@ import bookSeed from "./seeds/book.json" assert { type: "json" };
 import readerSeed from "./seeds/reader.json" assert { type: "json" };
 
 async function seed() {
+  // Create readers
   await Reader.bulkCreate(readerSeed, {
+    // Be sure to sanitize the data before seeding
     individualHooks: true,
   });
 
+  // Get all readers that were just seeded
   const readers = await Reader.findAll();
 
+  // Use Promise.all when you need to wait for multiple promises to resolve
   Promise.all(
+    // Join each seeded reader with a card
     readers.map(async (reader) => {
       await Card.create({ readerId: reader.id });
     })
@@ -21,7 +26,10 @@ async function seed() {
   Promise.all(
     bookSeed.map(async (book) => {
       await Book.create({
+        // Create a book based on the seed data
         ...book,
+
+        // Join the book to a random reader
         readerId: readers[Math.floor(Math.random() * readers.length)].id,
       });
     })
@@ -29,17 +37,16 @@ async function seed() {
 }
 
 export default async function init() {
+  // By default, Sequelize will use onDelete: "NOT NULL"
   Reader.hasOne(Card, {
     // Delete child 🧒🏾 records when parent is deleted
     onDelete: "CASCADE",
   });
-
   Card.belongsTo(Reader);
-
   Reader.hasMany(Book);
-
   Book.belongsTo(Reader);
 
+  // Sync all models that are defined
   await sequelize.sync().catch((err) => {
     console.error(`Error syncing database: ${err}`);
     process.exit(1);
@@ -49,6 +56,7 @@ export default async function init() {
 
   console.info("Checking if database needs to be seeded...");
 
+  // Only seed if there are no readers
   const readers = await Reader.findAll();
   if (!readers.length) {
     console.info("Database needs to be seeded.");
